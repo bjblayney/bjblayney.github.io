25556b1e108f120072881d57bc2986dd11d3276f
bjblayney <bj.blayney@gmail.com>
2021-01-02 19:37:50 -0500

    removing panes

--- src/components/tip.js
+++ src/components/tip.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/tip.css";
 
-import { Pane, Heading, TextInputField } from "evergreen-ui";
+import { Heading, TextInputField } from "evergreen-ui";
 
 function Tip() {
   const [withTip, setWithTip] = useState(0);
@@ -16,76 +16,60 @@ function Tip() {
 
   return (
     <div className="tipForm">
-      <Pane
-        display="flex"
-        justifyContent="space-around"
-        flexDirection="column"
-        padding={24}
-        background="tint2"
-        borderRadius={3}
-        margin={12}
-      >
-        <Pane display="flex">
-          <Heading size={800} paddingBottom={16}>
-            Tip well.
-          </Heading>
-        </Pane>
-        <Pane display="flex">
-          <TextInputField
-            label="Bill Total"
-            placeholder="0"
-            value={total}
-            onChange={(e) => {
-              calculateTip(e.target.value, tip / 100);
-              setTotal(e.target.value);
-            }}
-          />
-        </Pane>
-        <Pane display="flex">
-          <TextInputField
-            label="Tip Percentage"
-            hint="Default 20%"
-            placeholder="20"
-            type="number"
-            value={tip}
-            onChange={(e) => {
-              calculateTip(total, e.target.value / 100);
-              setTip(e.target.value);
-            }}
-          />
-        </Pane>
-        <Pane display="flex">
-          <TextInputField
-            label="# of Peeps"
-            placeholder="1"
-            type="number"
-            value={peeps < 1 ? 1 : peeps}
-            onChange={(e) => {
-              calculateTip(
-                total,
-                tip / 100,
-                e.target.value < 1 ? 1 : e.target.value
-              );
-              setPeeps(e.target.value < 1 ? 1 : e.target.value);
-            }}
-          />
-        </Pane>
-        <Pane display="flex">
-          <Heading size={800}>
-            Tip:{" "}
-            {parseFloat(
-              (parseFloat(total) * (parseFloat(tip) / 100)) / peeps
-            ).toFixed(2) > 0
-              ? parseFloat(
-                  (parseFloat(total) * (parseFloat(tip) / 100)) / peeps
-                ).toFixed(2)
-              : 0}
-          </Heading>
-        </Pane>
-        <Pane display="flex">
-          <Heading size={800}>Total: {parseFloat(withTip).toFixed(2)}</Heading>
-        </Pane>
-      </Pane>
+      <div className="item">
+        <TextInputField
+          label="Bill Total"
+          placeholder="0"
+          marginBottom={0}
+          value={total}
+          onChange={(e) => {
+            calculateTip(e.target.value, tip / 100);
+            setTotal(e.target.value);
+          }}
+        />
+
+        <TextInputField
+          label="Tip Percentage"
+          hint="Default 20%"
+          placeholder="20"
+          type="number"
+          marginBottom={0}
+          value={tip}
+          onChange={(e) => {
+            calculateTip(total, e.target.value / 100);
+            setTip(e.target.value);
+          }}
+        />
+        <TextInputField
+          label="# of Peeps"
+          placeholder="1"
+          type="number"
+          marginBottom={0}
+          value={peeps < 1 ? 1 : peeps}
+          onChange={(e) => {
+            calculateTip(
+              total,
+              tip / 100,
+              e.target.value < 1 ? 1 : e.target.value
+            );
+            setPeeps(e.target.value < 1 ? 1 : e.target.value);
+          }}
+        />
+      </div>
+      <div className="item">
+        <Heading size={800}>
+          Tip:{" "}
+          {parseFloat(
+            (parseFloat(total) * (parseFloat(tip) / 100)) / peeps
+          ).toFixed(2) > 0
+            ? parseFloat(
+                (parseFloat(total) * (parseFloat(tip) / 100)) / peeps
+              ).toFixed(2)
+            : 0}
+        </Heading>
+
+        <Heading size={800}>Total: {parseFloat(withTip).toFixed(2)}</Heading>
+      </div>
     </div>
   );
 }
